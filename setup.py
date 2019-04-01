426ecb1616edd145ed708b1bd17a393c701a5937
hhyo <rtttte@qq.com>
2019-04-01 17:20:23 +0800

    support python3.4, update version='0.1.6'

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class Test(test):
 
 setup(
     name='mybatis-mapper2sql',
-    version='0.1.5',
+    version='0.1.6',
     author='hhyo',
     author_email='rtttte@qq.com',
     url='http://github.com/hhyo/mybatis-mapper2sql',
